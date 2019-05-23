d3bf18e324462d317581f40c22c786c39c6eeff4
Richard Wilkes <wilkes@me.com>
2019-05-23 09:25:46 -0700

    Add CGColorSpaceCreatePattern()

--- cgcolorspace_darwin.go
+++ cgcolorspace_darwin.go
@@ -9,6 +9,10 @@ func CGColorSpaceCreateDeviceRGB() CGColorSpace {
 	return C.CGColorSpaceCreateDeviceRGB()
 }
 
+func CGColorSpaceCreatePattern(baseSpace CGColorSpace) CGColorSpace {
+	return C.CGColorSpaceCreatePattern(baseSpace)
+}
+
 func (cs CGColorSpace) Release() {
 	C.CGColorSpaceRelease(cs)
 }
