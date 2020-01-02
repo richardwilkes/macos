30f7f4f306b887c94057b7d782681284ad75b842
Richard Wilkes <wilkes@me.com>
2020-01-02 09:17:31 -0800

    Add copyright

--- gen/codegen_darwin.go
+++ gen/codegen_darwin.go
@@ -1,3 +1,12 @@
+// Copyright ©2019-2020 by Richard A. Wilkes. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, version 2.0.
+
 package main
 
 //go:generate go run codegen_darwin.go
@@ -98,7 +107,7 @@ func main() {
 
 func processTemplate(name string, arg interface{}) {
 	var buffer bytes.Buffer
-	fmt.Fprintf(&buffer, "// Code generated from \"tmpl/%s.go.tmpl\" - DO NOT EDIT.\n", name)
+	fmt.Fprintf(&buffer, "// Code generated from \"tmpl/%s.go.tmpl\" - DO NOT EDIT.\n//\n", name)
 	tmpl, err := template.New(name + ".go.tmpl").Funcs(template.FuncMap{
 		"firstToLower": firstToLower,
 		"firstToUpper": firstToUpper,
